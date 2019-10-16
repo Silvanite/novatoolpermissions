f8338c282cefa8326be87f55a3f8a590357f781b
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2019-10-16 15:07:11 +0100

    delete the correct policy, today has been long...

---
+++ src-php/Traits/IsAccessible.php
@@ -0,0 +1,47 @@
+<?php
+namespace Silvanite\NovaToolPermissions\Traits;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Input;
+use Silvanite\NovaToolPermissions\Access;
+trait IsAccessible
+{
+    public function initializeIsAccessible()
+    {
+        $this->with = array_merge($this->with, [
+            'access',
+        ]);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::created(function ($model) {
+            $model->access()
+                ->create(['roles' => Arr::wrap(Input::get('access_roles', []))]);
+        });
+        static::deleting(function ($model) {
+            if ($model->access) {
+                $access = $model->access;
+                $access->delete();
+            }
+        });
+    }
+
+    public function access()
+    {
+        return $this->morphOne(Access::class, 'accessible');
+    }
+ 
+    public function getAccessRolesAttribute()
+    {
+        return $this->access->roles ?? [];
+    }
+
+    public function setAccessRolesAttribute($roles)
+    {
+        if ($access = $this->access) {
+            $access->roles = $roles;
+            return $access->save();
+        }
+    }
+}
--- src-php/Traits/IsProtectablePolicy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Silvanite\NovaToolPermissions\Traits;
-
-trait IsProtectablePolicy
-{
-
-}
